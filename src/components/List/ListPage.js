33fa2d55e4d0da5d7103b96023e69ef24c5aefb4
jsgarzi <jsgarzi@fortitudedevelopment.com>
2021-03-01 16:53:54 -0500

    listing works

--- src/components/List/ListPage.js
+++ src/components/List/ListPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import API from "../../utils/API";
+import Navbar from "../Navbar/Navbar";
 
 class ListPage extends Component {
 
@@ -19,8 +20,36 @@ class ListPage extends Component {
     render() {
         return (
             <div>
-
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First Name</th>
+                            <th scope="col">Last Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Location</th>
+                            <th scope="col">Age</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.exportingData.map((e,i) => {
+                            return (
+                                <tr>
+                                    <th scope="row">{i + 1}</th>
+                                    <td>{e.name.first}</td>
+                                    <td>{e.name.last}</td>
+                                    <td>{e.email}</td>
+                                    <td>{e.location.state}</td>
+                                    <td>{e.dob.age}</td>
+                                </tr>
+                            )
+                            })
+                        }
+                    </tbody>
+                </table>
             </div>
         )
     }
 }
+
+export default ListPage
